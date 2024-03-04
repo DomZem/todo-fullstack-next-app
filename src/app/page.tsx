610c75652e75973a9a783979ed070783ba31bf70
Dominik <dominik.zemlik.dev@gmail.com>
2024-03-04 21:19:10 +0100

    initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <div>
+        <h1>Hello there</h1>
+      </div>
+    </main>
+  );
+}
